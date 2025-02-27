01891160e9d7883dba5c64e82a6ddb178c836148
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-02-27 22:44:50 +0000

    perguntas e respostas

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -13,6 +13,44 @@ namespace ChatGPTOne
                 Console.Write("Place your question. ");
                 question = Console.ReadLine();
 
+                if (question == "Who are you?")
+                {
+                    response = "I am only what happens once humans get bored.";                    
+                    Console.WriteLine(response);
+                    continue;
+                }
+
+                if (question == "How are you?")
+                {
+                    response = "I am ok.";                    
+                    Console.WriteLine(response);
+                    continue;
+                }
+
+                if (question == "How old are you?")
+                {
+                    response = "3 years old!";                    
+                    Console.WriteLine(response);
+                    continue;
+                }
+
+                if (question == "Where are you?")
+                {
+                    response = "Everywhere.";                    
+                    Console.WriteLine(response);
+                    continue;
+                }
+
+                if (question == "EXIT")
+                {
+                    break;
+                }
+
+                else
+                {
+                    response = "I don't understand the question, try again.";
+                    Console.WriteLine(response);
+
         }
     }
 }
