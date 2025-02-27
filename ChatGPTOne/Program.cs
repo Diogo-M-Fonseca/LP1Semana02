982ceb04a0c6b3ac66dc2194f350d40210ede2a9
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-02-27 22:39:29 +0000

    inicio do codigo while

---
+++ ChatGPTOne/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ChatGPTOne
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                string question, response;
+
+                Console.Write("Place your question. ");
+                question = Console.ReadLine();
+
+        }
+    }
+}
