3f9dc8e46c25fe7b226d17a8e6d60847cf73e138
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-02-27 10:21:31 +0000

    comments

--- SmoothOperator/Program.cs
+++ SmoothOperator/Program.cs
@@ -8,10 +8,10 @@ namespace SmoothOperator
         private static void Main(string[] args)
         {
             Console.WriteLine("Escreva um numero inteiro");
-            string read = Console.ReadLine();
-            sbyte bread = sbyte.Parse(read);
-            Console.WriteLine(--bread);
-            Console.WriteLine(++bread);
+            string read = Console.ReadLine(); // read line 
+            sbyte bread = sbyte.Parse(read); // converts to sbyte
+            Console.WriteLine(--bread); // decrement sbyte variable
+            Console.WriteLine(++bread); // increment sbyte variable
         }
     }
 }
