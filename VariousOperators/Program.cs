851aa8bde30fa36a28ba7950eea32f5dbad684d4
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-02-27 20:44:05 +0000

    comentarios no novo codigo

--- VariousOperators/Program.cs
+++ VariousOperators/Program.cs
@@ -9,12 +9,12 @@ namespace VariousOperators
             Console.WriteLine("Inserir um número inteiro-não negativo: ");
 
             string read = Console.ReadLine();
-            byte num = byte.Parse(read);
+            byte num = byte.Parse(read); // byte.parse converte string em byte
 
-            Console.WriteLine(num/2);
-            Console.WriteLine(num<<3);
-            Console.WriteLine(num^6);
-            Console.WriteLine(num>10);
+            Console.WriteLine(num/2); // divisão inteira
+            Console.WriteLine(num<<3); // shift left
+            Console.WriteLine(num^6); // xor
+            Console.WriteLine(num>10); // maior que o operador
         }
     }
 }
