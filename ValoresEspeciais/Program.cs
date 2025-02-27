4749a5e2029271e3edf996b48c265ca0ee319ed8
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-02-27 21:30:21 +0000

    valores especiais float and double

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ValoresEspeciais
 {
@@ -6,7 +7,39 @@ namespace ValoresEspeciais
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+
+                //Todos os valores maximos
+            Console.WriteLine(sbyte.MaxValue);
+            Console.WriteLine(byte.MaxValue);
+            Console.WriteLine(uint.MaxValue);
+            Console.WriteLine(long.MaxValue);
+            Console.WriteLine(float.MaxValue);
+            Console.WriteLine(double.MaxValue);
+            Console.WriteLine(decimal.MaxValue);
+            Console.WriteLine(Int128.MaxValue);
+            Console.WriteLine(short.MaxValue);
+            Console.WriteLine(ulong.MaxValue);
+
+                //Todos os valores minimos
+            Console.WriteLine(sbyte.MinValue);
+            Console.WriteLine(byte.MinValue);
+            Console.WriteLine(short.MinValue);
+            Console.WriteLine(long.MinValue);
+            Console.WriteLine(float.MinValue);
+            Console.WriteLine(ulong.MinValue);
+            Console.WriteLine(double.MinValue);
+            Console.WriteLine(Int128.MinValue);
+            Console.WriteLine(uint.MinValue);
+            Console.WriteLine(decimal.MinValue);
+
+            //Valores especiais float and double
+            Console.WriteLine(float.PositiveInfinity);
+            Console.WriteLine(float.NegativeInfinity);
+            Console.WriteLine(float.NaN);
+            
+            Console.WriteLine(double.PositiveInfinity);
+            Console.WriteLine(double.NegativeInfinity);
+            Console.WriteLine(double.NaN);
         }
     }
 }
