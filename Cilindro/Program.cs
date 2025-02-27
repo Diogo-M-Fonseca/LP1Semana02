574f4424ea285dce9220b653ea80f78e408ae20c
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-02-27 21:02:57 +0000

    calculo da superficie

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 
 namespace Cilindro
 {
@@ -6,7 +7,21 @@ namespace Cilindro
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Console.WriteLine("Inserir altura do cilindro: ");
+            string rread = Console.ReadLine();
+            Console.WriteLine("Inserir raio do cilindro: ");
+            string read = Console.ReadLine();
+
+            double altura = double.Parse(rread);
+            double raio = double.Parse(read);
+
+            double v = Math.PI * Math.Pow(raio,2)*altura;
+
+
+            double sa = 2*Math.PI*raio*(raio + altura);
+
+
+
         }
     }
 }
