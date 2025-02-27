9d0e29c4a356a3baae5978972930b2a9f2cfbfec
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-02-27 20:39:21 +0000

    primeira tentativa

---
+++ VariousOperators/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VariousOperators
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Inserir um número inteiro-não negativo: ");
+
+            string read = Console.ReadLine();
+            byte num = byte.Parse(read);
+
+            Console.WriteLine(num/2);
+            Console.WriteLine(num<<3);
+            Console.WriteLine(num^6);
+            Console.WriteLine(num>10);
+        }
+    }
+}
