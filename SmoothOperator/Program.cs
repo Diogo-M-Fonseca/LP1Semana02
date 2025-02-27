ba349addc0144f8d552c68b2b9fabd6cfbe0e83c
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-02-27 08:54:22 +0000

    first try ex1

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva um numero inteiro");
+            string read = Console.ReadLine();
+            sbyte bread = sbyte.Parse(read);
+            Console.WriteLine(--bread);
+            Console.WriteLine(++bread);
+        }
+    }
+}
