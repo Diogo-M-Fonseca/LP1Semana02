e18066964d42895894d8895c55cacb5e302444d4
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-02-28 17:34:38 +0000

    first try

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -6,7 +6,38 @@ namespace GuessTheNumber
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+           int tentativa = 0;
+            while(true)
+            {
+                
+                Random rnd = new Random();
+                int numberToGuess = rnd.Next(31);
+
+                Console.WriteLine("inserir um numero inteiro entre 0 e 30: ");
+                string read = Console.ReadLine();
+                int rread = int.Parse(read);
+                 
+                if (rread < 0 )
+                {
+                    Console.WriteLine("O numero escolhido é menor que zero");
+                }
+                else if (rread > 30)
+                {
+                    Console.WriteLine("o numero escolhido é maior que 30");
+                }
+                else if (rread > numberToGuess)
+                {
+                    Console.WriteLine("o numero é maior que o numero aleatorio");
+                    tentativa += 1;
+                }
+                else if (rread < numberToGuess)
+                {
+                    Console.WriteLine("o numero é menor que o numero aleatorio");
+                    tentativa += 1;
+                }
+                
+            }
+            }
         }
     }
 }
